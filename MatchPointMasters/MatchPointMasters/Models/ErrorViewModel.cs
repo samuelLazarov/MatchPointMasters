1c6fd4f84e6b119272266501b4e463aeb3fcf2ee
SamuelLazarov <samuel.lazarov@gmail.com>
2024-03-22 09:17:02 +0200

    Add project files.

---
+++ MatchPointMasters/MatchPointMasters/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MatchPointMasters.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
