f47febc0414aa55b572fca07349631dd2770a776
SamuelLazarov <samuel.lazarov@gmail.com>
2024-03-22 11:37:40 +0200

    ModelBinder added ClaimsPrincipals added MainMenuComponent added

---
+++ MatchPointMasters/MatchPointMasters/Extensions/ClaimsPrincipalsExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace MatchPointMasters.Extensions
+{
+    public static class ClaimsPrincipalsExtension
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
--- MatchPointMasters/MatchPointMasters/Program.cs
+++ MatchPointMasters/MatchPointMasters/Program.cs
@@ -1,10 +1,15 @@
 
+using MatchPointMasters.ModelBinders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity(builder.Configuration);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+{
+    options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+});
 
 builder.Services.AddApplicationServices();
 
