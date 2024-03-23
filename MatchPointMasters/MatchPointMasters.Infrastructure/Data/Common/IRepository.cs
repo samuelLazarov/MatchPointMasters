dc2c5cbc7b607614df1dc37f41b9bd5d086b59df
SamuelLazarov <samuel.lazarov@gmail.com>
2024-03-23 10:03:52 +0200

    Repository added

---
+++ MatchPointMasters/MatchPointMasters.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace MatchPointMasters.Infrastructure.Data.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> AllReadOnly<T>() where T : class;
+
+        Task AddAsync<T>(T entity) where T : class;
+
+        Task<int> SaveChangesAsync();
+
+        Task<T?> GetByIdAsync<T>(object id) where T : class;
+
+    }
+}
